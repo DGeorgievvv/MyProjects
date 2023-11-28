b44b33b99d55312095e811f56f9bb15d08b9bf00
DGeorgievvv <116742042+DGeorgievvv@users.noreply.github.com>
2023-11-28 23:14:05 +0200

    Add files via upload

---
+++ Squares in matrix/KursowaRabota_zad3/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KursowaRabota_zad3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the size of the matrix: ");
+            int n = int.Parse(Console.ReadLine());
+            int[,] matrix = new int[n, n];
+
+            for(int i = 0; i < n; i++)
+            {
+                string[] rowValues = Console.ReadLine().Split();
+                for(int j = 0; j < n; j++)
+                {
+                    matrix[i, j] = int.Parse(rowValues[j]);
+                }
+            }
+
+            int sum = 0;
+            int[] perimeterSum = new int[2 * (n - 1) + 2 * (n - 2)];
+
+            for (int i = 0; i < n - 1; i++)
+            {
+                for (int j = i; j < n - 1; j++)
+                {
+                    sum += matrix[i, j];
+                    sum += matrix[n - 1 - i, j];
+                    if (i != 0 && i != n - 1)
+                    {
+                        sum += matrix[j, i];
+                        sum += matrix[j, n - 1 - i];
+                    }
+                }
+            }
+
+            int index = 0;
+            for(int i = 0; i < n - 1; i++)
+            {
+                for(int j = i; j < n - i; j++)
+                {
+                    perimeterSum[index++] = sum;
+                }
+            }
+
+            bool isMonotonic = true;
+            for(int i = 1; i < perimeterSum.Length; i++)
+            {
+                if(perimeterSum[i] < perimeterSum[i - 1])
+                {
+                    isMonotonic = false;
+                    break;
+                }
+            }
+
+            Console.WriteLine($"The sum of perimeter is : {sum}");
+
+            if(isMonotonic)
+            {
+                Console.WriteLine("The row is monotonic.");
+            }
+            else
+            {
+                Console.WriteLine("The row is not monotonic.");
+            }
+        }
+    }
+}
---
+++ TreeDepth/KursowaRabota_zad15/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KursowaRabota_zad15
+{
+    public class Node
+    {
+        public int data;
+        public Node left, right;
+
+        public Node(int item)
+        {
+            data = item;
+            left = null;
+            right = null;
+        }
+    }
+
+    public class BinaryTree
+    {
+        public Node root;
+
+        private int FindTreeDepth(Node node)
+        {
+            if(node == null)
+            {
+                return 0;
+            }
+            else
+            {
+                int leftDepth = FindTreeDepth(node.left);
+                int rightDepth = FindTreeDepth(node.right);
+                if(leftDepth >= rightDepth)
+                {
+                    return leftDepth + 1;
+                }
+                else
+                {
+                    return rightDepth + 1;
+                }
+            }
+        }
+
+        public int TreeDepth()
+        {
+            return FindTreeDepth(root);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BinaryTree tree = new BinaryTree();
+            tree.root = new Node(1);
+            tree.root.left = new Node(2);
+            tree.root.right = new Node(3);
+            tree.root.left.left = new Node(4);
+            tree.root.left.right = new Node(5);
+
+            int depth = tree.TreeDepth();
+            Console.WriteLine($"Tree depth is : {depth}");
+        }
+    }
+}
